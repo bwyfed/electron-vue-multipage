cfcf72b8fa5e3a25a9e1b2dc9f8025cefcb4f7d7
Bianwy <bianwy025@qq.com>
2020-03-21 13:47:23 +0800

    '实现windows下打包的基本配置，并测试可成功打包。'

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,28 @@ module.exports = {
   pages: {
     index: "src/main.js",
     subpage: "src/subpage/main.js"
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.geektime.mercurius",
+        productName: "Mercurius",
+        copyright: "Copyright © 2020 bian",
+        directories: {
+          // output: 'release'
+        },
+        asar: false,
+        win: {
+          icon: "resources/icon.ico",
+          target: ["nsis"]
+        },
+        nsis: {
+          oneClick: false,
+          language: "2052",
+          perMachine: true,
+          allowToChangeInstallationDirectory: true
+        }
+      }
+    }
   }
 };
