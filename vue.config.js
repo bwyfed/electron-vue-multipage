8c0ccc3e6fb2ff7555d82414432bb2546ae1bcf1
Bianwy <bianwy025@qq.com>
2020-03-21 13:56:14 +0800

    '增加webpack的配置。'

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
+      chainWebpackRendererProcess: config => {
+        config.plugin("define").tap(args => {
+          args[0]["IS_ELECTRON"] = true;
+          return args;
+        });
+      },
       builderOptions: {
         appId: "com.geektime.mercurius",
         productName: "Mercurius",
