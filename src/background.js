57d64c54823e7efc4099d0aa632f2b153a5fb6a3
Bianwy <bianwy025@qq.com>
2020-03-21 13:10:14 +0800

    '实现多窗口的功能。'

--- src/background.js
+++ src/background.js
@@ -10,15 +10,17 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+let secondWin;
+let createAppProtocol = false;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
 ]);
 
-function createWindow() {
+function createWindow(winVar, devPath, prodPath) {
   // Create the browser window.
-  win = new BrowserWindow({
+  winVar = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -28,16 +30,19 @@ function createWindow() {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    winVar.loadURL(process.env.WEBPACK_DEV_SERVER_URL + devPath);
+    if (!process.env.IS_TEST) winVar.webContents.openDevTools();
   } else {
-    createProtocol("app");
+    if (!createAppProtocol) {
+      createProtocol("app");
+      createAppProtocol = true;
+    }
     // Load the index.html when not in development
-    win.loadURL("app://./index.html");
+    winVar.loadURL(`app://./${prodPath}`);
   }
 
-  win.on("closed", () => {
-    win = null;
+  winVar.on("closed", () => {
+    winVar = null;
   });
 }
 
@@ -54,7 +59,10 @@ app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow();
+    createWindow(win, "", "index.html");
+  }
+  if (secondWin === null) {
+    createWindow(secondWin, "subpage", "subpage.html");
   }
 });
 
@@ -75,7 +83,8 @@ app.on("ready", async () => {
     //   console.error('Vue Devtools failed to install:', e.toString())
     // }
   }
-  createWindow();
+  createWindow(win, "", "index.html");
+  createWindow(secondWin, "subpage", "subpage.html");
 });
 
 // Exit cleanly on request from parent process in development mode.
