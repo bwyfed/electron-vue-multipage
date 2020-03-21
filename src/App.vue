882519a26fda7ce73e371783a45d571bc5e3cf25
Bianwy <bianwy025@qq.com>
2020-03-21 11:48:10 +0800

    '增加element-ui。'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <el-button type="primary">主要按钮</el-button>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
